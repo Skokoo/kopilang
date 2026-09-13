39c7ec0aeafcec83f3c78b5a1fb4ac8f1aaa183e
Skokoo <nourriwilliam789@gmail.com>
2026-09-13 16:46:52 +0700

    Array time is not my favorite party

--- syntax/x86_syn.h
+++ syntax/x86_syn.h
@@ -23,9 +23,9 @@ void x86_put(char *s) {
 }
 
 void x86_num(long v) {
-    char bf;
+    char bf[32];
     int i = 30;
-    bf = '\n';
+    bf[31] = '\n';
     long tm = v;
     if (v < 0) tm = -v;
     do {
@@ -46,7 +46,7 @@ void x86_num(long v) {
 }
 
 long x86_get() {
-    char bf = {0};
+    char bf[32] = {0};
     long vl = 0;
     int sg = 1;
     long ln = 0;
@@ -72,7 +72,7 @@ long x86_get() {
 }
 
 void do_int_x86(char *fn, char *rw, char *cm, Var *db, int *cn, int l, int *er) {
-    char nm = {0}, ex = {0};
+    char nm[64] = {0}, ex[256] = {0};
     if (sscanf(cm, "INTEGER %63s = %255[^\n]", nm, ex) != 2) {
         printf("\033[1m%s:%d:1: \033[1;31merror:\033[0m\033[1m invalid INTEGER format\033[0m\n", fn, l);
         printf(" |\n | %s\n |\n", rw);
@@ -88,7 +88,7 @@ void do_int_x86(char *fn, char *rw, char *cm, Var *db, int *cn, int l, int *er) 
     else if (strchr(cex, '/')) op = '/';
 
     if (op != 0) {
-        char a1 = {0}, a2 = {0}, fm;
+        char a1[64] = {0}, a2[64] = {0}, fm[32];
         sprintf(fm, "%%63s %c %%63s", op);
         sscanf(cex, fm, a1, a2);
         int i1 = get_idx(db, *cn, trim(a1));
@@ -126,17 +126,19 @@ void do_say_x86(char *fn, char *rw, char *cm, Var *db, int cn, int l, int *er) {
         return;
     }
     int rl = q2 - q1 - 1;
-    char ct = {0};
+    char ct[512] = {0};
     if (rl > 510) rl = 510;
     strncpy(ct, q1 + 1, rl);
-    static char pr;
+    
+    // PERBAIKAN FATAL: Menambahkan alokasi ukuran array siku [512]
+    static char pr[512];
     memset(pr, 0, sizeof(pr));
     int pi = 0;
     for (int i = 0; ct[i] != '\0'; i++) {
         if (ct[i] == '\\' && ct[i+1] == 'e') { pr[pi++] = 0x1B; i++; }
         else if (ct[i] == '\\' && ct[i+1] == 'n') { pr[pi++] = '\n'; i++; }
         else if (ct[i] == '!') {
-            char tg = {0};
+            char tg[64] = {0};
             int vl = 0;
             int st = i + 1;
             while (ct[st] != '\0' && !isspace((unsigned char)ct[st]) && ct[st] != '\\' && ct[st] != '"' && vl < 63) {
@@ -145,7 +147,7 @@ void do_say_x86(char *fn, char *rw, char *cm, Var *db, int cn, int l, int *er) {
             tg[vl] = '\0';
             int id = get_idx(db, cn, tg);
             if (id != -1) {
-                char nb;
+                char nb[32];
                 sprintf(nb, "%ld", db[id].val);
                 for (int b = 0; nb[b] != '\0'; b++) pr[pi++] = nb[b];
                 i += vl;
@@ -156,7 +158,7 @@ void do_say_x86(char *fn, char *rw, char *cm, Var *db, int cn, int l, int *er) {
 }
 
 void do_if_x86(char *fn, char *rw, char *cm, Var *db, int cn, int l, int *er, int *ab) {
-    char cp = {0};
+    char cp[256] = {0};
     if (sscanf(cm, "IF %[^\n]", cp) != 1) {
         printf("\033[1m%s:%d:1: \033[1;31merror:\033[0m\033[1m invalid IF syntax\033[0m\n", fn, l);
         *er = 1;
@@ -171,7 +173,7 @@ void do_if_x86(char *fn, char *rw, char *cm, Var *db, int cn, int l, int *er, in
     }
     cc[ln - 1] = '\0';
     cc = trim(cc);
-    char vn = {0}, op = {0};
+    char vn[64] = {0}, op[8] = {0};
     long vv = 0;
     if (strstr(cc, "==")) { strcpy(op, "=="); sscanf(cc, "%63s == %ld", vn, &vv); }
     else if (strstr(cc, "!=")) { strcpy(op, "!="); sscanf(cc, "%63s != %ld", vn, &vv); }
@@ -197,11 +199,11 @@ void do_input_x86(char *fn, char *rw, char *cm, Var *db, int cn, int l, int *er)
     char *q2 = strrchr(cm, '"');
     if (!q1 || !q2 || q1 == q2) { printf("\033[1m%s:%d:1: \033[1;31merror:\033[0m\033[1m matching quotes required for INPUT\033[0m\n", fn, l); *er = 1; return; }
     int ln = q2 - q1 - 1;
-    char tg = {0};
+    char tg[256] = {0};
     if (ln > 255) ln = 255;
     strncpy(tg, q1 + 1, ln);
     char *ctg = trim(tg);
-    if (ctg != '!') { printf("\033[1m%s:%d:1: \033[1;31merror:\033[0m\033[1m INPUT target must start with '!'\033[0m\n", fn, l); *er = 1; return; }
+    if (ctg[0] != '!') { printf("\033[1m%s:%d:1: \033[1;31merror:\033[0m\033[1m INPUT target must start with '!'\033[0m\n", fn, l); *er = 1; return; }
     char *vn = trim(ctg + 1);
     int id = get_idx(db, cn, vn);
     if (id == -1) { printf("\033[1m%s:%d:1: \033[1;31mruntime error:\033[0m\033[1m variable '%s' undefined\033[0m\n", fn, l, vn); *er = 1; return; }
